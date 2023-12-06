9cf90ddb296c03bc487b7f839652dc582c8bee43
SasankaPasanjith <sasankapasanjith@gmail.com>
2023-12-06 12:27:38 +0530

    Amended Admin Services.

--- src/main/java/com/TheJobsConsulting/service/AdminService.java
+++ src/main/java/com/TheJobsConsulting/service/AdminService.java
@@ -1,11 +1,27 @@
 package com.TheJobsConsulting.service;
 
+import com.TheJobsConsulting.entity.Appointment;
 import com.TheJobsConsulting.entity.Consultant;
+import com.TheJobsConsulting.entity.User;
+import com.TheJobsConsulting.exception.AppointmentException;
 import com.TheJobsConsulting.exception.ConsultantException;
+import com.TheJobsConsulting.exception.UserException;
 
 import java.util.List;
 
 public interface AdminService {
     Consultant registerConsultant(Consultant consultant) throws ConsultantException; //register consultant
     List <Consultant> getAllConsultants() throws ConsultantException;  //retrieve list of all consultant
+
+    Consultant revokePermissionConsultant (Consultant consultant) throws ConsultantException;  //consultant revoke permission
+
+    Consultant grantPermissionConsultant (Consultant consultant) throws ConsultantException; //consultant grant permission
+
+    List<User> getAllUsers()throws UserException;   //retrieve list of all Users
+    List<Appointment> getAllAppointments()throws AppointmentException; //retrieve list of all Appointments
+    List<Consultant> getAllValidInvalidConsultants(String key) throws ConsultantException; //retrieve list of all valid
+                                                                                           // & invalid Consultants
 }
+
+
+
--- src/main/java/com/TheJobsConsulting/service/AdminServiceImplement.java
+++ src/main/java/com/TheJobsConsulting/service/AdminServiceImplement.java
@@ -1,15 +1,18 @@
 package com.TheJobsConsulting.service;
 
 import com.TheJobsConsulting.config.SpringDocConfig;
+import com.TheJobsConsulting.entity.Appointment;
 import com.TheJobsConsulting.entity.Consultant;
+import com.TheJobsConsulting.entity.User;
+import com.TheJobsConsulting.exception.AppointmentException;
 import com.TheJobsConsulting.exception.ConsultantException;
+import com.TheJobsConsulting.exception.UserException;
 import com.TheJobsConsulting.repository.AppointmentDAO;
 import com.TheJobsConsulting.repository.ConsultantDAO;
 import com.TheJobsConsulting.repository.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.net.ConnectException;
 import java.util.List;
 
 @Service
@@ -35,7 +38,7 @@ public class AdminServiceImplement implements AdminService {
 
     @Override
     public List<Consultant> getAllConsultants() throws ConsultantException {
-        List<Consultant> listOfConsultant = consultantDAO.findAll();   //retrieve all consultants in DB using DAO
+        List<Consultant> listOfConsultant = consultantDAO.findAll();   //retrieve all consultants in DB using data access object
         if (!listOfConsultant.isEmpty()){
             return listOfConsultant;
         }
@@ -43,5 +46,46 @@ public class AdminServiceImplement implements AdminService {
             throw new ConsultantException("There are No Consultants");
         }
     }
+
+    @Override
+    public List<User> getAllUsers() throws UserException {       //returns a list of User objects
+        List<User>listUser = userDAO.findAll();
+        if (!listUser.isEmpty()){
+            return listUser;
+        }else {
+            throw new UserException("There are no any Users to Show.");
+        }
+    }
+
+    @Override
+    public List<Appointment> getAllAppointments() throws AppointmentException {  //returns a list of Appointment objects
+        List<Appointment>listAppointment = appointmentDAO.findAll();
+        if (!listAppointment.isEmpty()){
+            return listAppointment;
+        }else {
+            throw new AppointmentException("There are no Appointments to Show.");
+        }
+    }
+
+    @Override
+    public List<Consultant> getAllValidInvalidConsultants(String key) throws ConsultantException {  //returns a list of Consultant objects
+        List<Consultant>listConsultant = consultantDAO.findAll();
+        if (!listConsultant.isEmpty()){
+            return listConsultant;
+        }else{
+            throw new ConsultantException("There are no Consultants to Show");
+        }
+    }
+
+    @Override
+    public Consultant revokePermissionConsultant(Consultant consultant) throws ConsultantException {
+        return null;
+    }
+
+    @Override
+    public Consultant grantPermissionConsultant(Consultant consultant) throws ConsultantException {
+        return null;
+    }
+
 }
 
