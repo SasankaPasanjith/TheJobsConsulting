88ee85de53a6bfb5a49bf3ba4e737d8bf794faa8
SasankaPasanjith <sasankapasanjith@gmail.com>
2023-12-11 12:41:25 +0530

    Added methods to Consultant service implement

--- src/main/java/com/TheJobsConsulting/service/ConsultantService.java
+++ src/main/java/com/TheJobsConsulting/service/ConsultantService.java
@@ -1,8 +1,17 @@
 package com.TheJobsConsulting.service;
 
 import com.TheJobsConsulting.entity.Consultant;
+import com.TheJobsConsulting.entity.CurrentSession;
+import com.TheJobsConsulting.exception.ConsultantException;
+import com.TheJobsConsulting.exception.LoginException;
 import com.TheJobsConsulting.exception.UserException;
+
+import java.util.List;
 
 public interface ConsultantService {
     Consultant getConsultantDetails (String key)throws UserException;
+    CurrentSession getCurrentUserByUuid (String uuid) throws LoginException;
+    Consultant getConsultantByUuid(String uuid) throws UserException;
+    List<Consultant> getAllConsultantsInDb() throws ConsultantException;
+
 }
--- src/main/java/com/TheJobsConsulting/service/ConsultantServiceImplement.java
+++ src/main/java/com/TheJobsConsulting/service/ConsultantServiceImplement.java
@@ -2,12 +2,15 @@ package com.TheJobsConsulting.service;
 
 import com.TheJobsConsulting.entity.Consultant;
 import com.TheJobsConsulting.entity.CurrentSession;
+import com.TheJobsConsulting.exception.ConsultantException;
+import com.TheJobsConsulting.exception.LoginException;
 import com.TheJobsConsulting.exception.UserException;
 import com.TheJobsConsulting.repository.ConsultantDAO;
 import com.TheJobsConsulting.repository.SessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,4 +32,39 @@ public class ConsultantServiceImplement implements ConsultantService {
             throw new UserException("There is No Consultant From this Key"+key);
         }
     }
+
+    @Override
+    public CurrentSession getCurrentUserByUuid(String uuid) throws LoginException { //find a user session based on the
+                                                                                    // provided UUID
+        CurrentSession currentUserSession = sessionDAO.findByUuid(uuid);
+        if (currentUserSession != null) {
+            return currentUserSession;
+        }else {
+            throw new LoginException ("Please Enter Valid Key.");
+        }
+    }
+
+    @Override
+    public Consultant getConsultantByUuid(String uuid) throws UserException {    //find a consultant session based on the
+                                                                                 // provided UUID
+        CurrentSession currentConsultant = sessionDAO.findByUuid(uuid);
+        Optional<Consultant> consultant = consultantDAO.findById(currentConsultant.getUserId());  //session to find
+                                                                                                  // a consultant by ID
+        if (consultant.isPresent()){
+            return consultant.get();
+        }else {
+            throw new UserException("No Consultants From This Key."+ uuid);
+        }
+    }
+
+    @Override
+    public List<Consultant> getAllConsultantsInDb() throws ConsultantException {   //retrieve all consultants from the database
+        List<Consultant> listConsultant = consultantDAO.findAll();
+        if (listConsultant.isEmpty()){
+            throw new ConsultantException("No Registered Consultants.");
+        }else {
+            return listConsultant;
+        }
+    }
+
 }
