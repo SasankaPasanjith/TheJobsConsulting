e3870dae44e24dea57b0c5d69cad976f33b512a0
SasankaPasanjith <sasankapasanjith@gmail.com>
2023-12-13 12:26:34 +0530

    Created Consultant Controller

--- src/main/java/com/TheJobsConsulting/controller/ConsultantController.java
+++ src/main/java/com/TheJobsConsulting/controller/ConsultantController.java
@@ -1,4 +1,36 @@
 package com.TheJobsConsulting.controller;
 
+
+import com.TheJobsConsulting.entity.Consultant;
+import com.TheJobsConsulting.exception.LoginException;
+import com.TheJobsConsulting.exception.UserException;
+import com.TheJobsConsulting.service.ConsultantLoginService;
+import com.TheJobsConsulting.service.ConsultantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/V3")
 public class ConsultantController {
+    @Autowired
+    ConsultantLoginService consultantLoginService;
+    @Autowired
+    ConsultantService consultantService;
+
+    @GetMapping("/consultantDetails")
+    @CrossOrigin
+    public ResponseEntity<Consultant>getConsultantDetails (@RequestParam String key)throws   //returns a ResponseEntity
+                                                                                       // containing a Consultant object
+            LoginException, UserException{
+        if (consultantLoginService.checkUserLogin(key)){                     //Check whether the consultant is logged in
+            Consultant returnConsultant = consultantService.getConsultantDetails(key); //calls a method to retrieve
+                                                                                       // details for the consultant
+            return new ResponseEntity<Consultant>(returnConsultant, HttpStatus.CREATED);
+        }else {
+            throw new LoginException("Please Enter Valid Key.");
+        }
+    }
 }
+
