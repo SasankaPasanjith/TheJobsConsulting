25a6aac5fc97bd89f1748303c57f2e63154980de
SasankaPasanjith <sasankapasanjith@gmail.com>
2023-12-24 12:20:31 +0530

    added Some User controller methods.

--- src/main/java/com/TheJobsConsulting/controller/UserController.java
+++ src/main/java/com/TheJobsConsulting/controller/UserController.java
@@ -1,18 +1,24 @@
 package com.TheJobsConsulting.controller;
 
+import com.TheJobsConsulting.entity.Appointment;
+import com.TheJobsConsulting.entity.Consultant;
 import com.TheJobsConsulting.entity.User;
-import com.TheJobsConsulting.exception.LoginException;
-import com.TheJobsConsulting.exception.UserException;
+import com.TheJobsConsulting.exception.*;
 import com.TheJobsConsulting.repository.SessionDAO;
 import com.TheJobsConsulting.repository.UserDAO;
+import com.TheJobsConsulting.service.ConsultantService;
 import com.TheJobsConsulting.service.UserAdminLoginService;
 import com.TheJobsConsulting.service.UserService;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.time.DateTimeException;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/V3")
@@ -26,6 +32,8 @@ public class UserController {
     SessionDAO sessionDAO;
     @Autowired
     UserAdminLoginService userAdminLoginService;
+    @Autowired
+    ConsultantService consultantService;
 
     @CrossOrigin
     @PostMapping("/registerUser")
@@ -52,7 +60,51 @@ public class UserController {
         }
     }
 
+    @PostMapping("/bookAppointment")
+    @CrossOrigin
+    public ResponseEntity<Appointment> bookAppointment (@RequestParam String key, @RequestBody Appointment appointment)
+            throws LoginException, AppointmentException, ConsultantException, IOException, DateTimeException,
+            MessagingException, TimeDateException {
+        if (appointment == null){
+            throw new AppointmentException("Please Enter Valid Appointment Details.");
+        }
+        if (userAdminLoginService.checkUserLogin(key)){
+            Appointment registerAppointment = userService.bookAppointment(key, appointment); //Proceeds to book appointment
+            return new ResponseEntity<Appointment>(registerAppointment, HttpStatus.CREATED);
+        }else {
+            throw new LoginException("Please Login Again.");
+        }
+    }
+
+    @GetMapping("/allConsultants")
+    @CrossOrigin
+    public ResponseEntity<List<Consultant>> getAllConsultantsInDatabase (@RequestParam String key) throws LoginException,
+            ConsultantException{
+        if (userAdminLoginService.checkUserLogin(key)){
+            List<Consultant> listOfConsultants = consultantService.getAllConsultantsInDb(); //Retrieve a list of consultants
+                                                                                            // from the database
+            return new ResponseEntity<List<Consultant>>(listOfConsultants, HttpStatus.ACCEPTED);
+        }else {
+            throw new LoginException("Invalid Credentials; Please Login.");
+        }
+    }
+
+    @GetMapping("/getAllConsultants")
+    @CrossOrigin
+    public ResponseEntity<List<Consultant>> getAllConsultants(@RequestParam String key) throws
+            LoginException, ConsultantException{
+        if (userAdminLoginService.checkUserLogin(key)){  //Retrieves list of consultants from the userService
+            List<Consultant> consultantList = userService.getAllConsultant();
+            return new ResponseEntity<List<Consultant>>(consultantList, HttpStatus.ACCEPTED);
+        }else {
+            throw new LoginException("Invalid Credentials; Please Login.");
+        }
+    }
+
+
 }
+
+
 
 
 
