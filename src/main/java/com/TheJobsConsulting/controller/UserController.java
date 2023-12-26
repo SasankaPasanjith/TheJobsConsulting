a951d0c5a6abbbacba1fcfa1fddb18b6cef376a3
SasankaPasanjith <sasankapasanjith@gmail.com>
2023-12-26 12:41:17 +0530

    added Some User controller methods.

--- src/main/java/com/TheJobsConsulting/controller/UserController.java
+++ src/main/java/com/TheJobsConsulting/controller/UserController.java
@@ -2,6 +2,7 @@ package com.TheJobsConsulting.controller;
 
 import com.TheJobsConsulting.entity.Appointment;
 import com.TheJobsConsulting.entity.Consultant;
+import com.TheJobsConsulting.entity.ForgotPassword;
 import com.TheJobsConsulting.entity.User;
 import com.TheJobsConsulting.exception.*;
 import com.TheJobsConsulting.repository.SessionDAO;
@@ -37,54 +38,54 @@ public class UserController {
 
     @CrossOrigin
     @PostMapping("/registerUser")
-    public ResponseEntity<User>saveUser(@Valid @RequestBody User user)throws UserException{
-        User savedUser =  userService.createUser(user);                    //Calls a service method to create a new user
+    public ResponseEntity<User> saveUser(@Valid @RequestBody User user) throws UserException {
+        User savedUser = userService.createUser(user);                    //Calls a service method to create a new user
         return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
     }
 
     @PutMapping("/updateUser")
-    public ResponseEntity<User>updateUser(@RequestBody User user, @RequestParam(required = false) String key) throws
-            UserException{                      //Calls a service method to update a user with the provided data and key
+    public ResponseEntity<User> updateUser(@RequestBody User user, @RequestParam(required = false) String key) throws
+            UserException {                      //Calls a service method to update a user with the provided data and key
         User updatedUser = userService.updateUser(user, key);
         return new ResponseEntity<User>(updatedUser, HttpStatus.OK);
     }
 
     @GetMapping("/viewUser")
     @CrossOrigin
-    public ResponseEntity<User>viewUserDetails(@RequestParam String key) throws LoginException, UserException{
-        if (userAdminLoginService.checkUserLogin(key)){
+    public ResponseEntity<User> viewUserDetails(@RequestParam String key) throws LoginException, UserException {
+        if (userAdminLoginService.checkUserLogin(key)) {
             User returnUser = userService.getUserDetails(key); // retrieve details about the user with the specified key
             return new ResponseEntity<User>(returnUser, HttpStatus.ACCEPTED);
-        }else {
+        } else {
             throw new LoginException("User Not Found.");
         }
     }
 
     @PostMapping("/bookAppointment")
     @CrossOrigin
-    public ResponseEntity<Appointment> bookAppointment (@RequestParam String key, @RequestBody Appointment appointment)
+    public ResponseEntity<Appointment> bookAppointment(@RequestParam String key, @RequestBody Appointment appointment)
             throws LoginException, AppointmentException, ConsultantException, IOException, DateTimeException,
             MessagingException, TimeDateException {
-        if (appointment == null){
+        if (appointment == null) {
             throw new AppointmentException("Please Enter Valid Appointment Details.");
         }
-        if (userAdminLoginService.checkUserLogin(key)){
+        if (userAdminLoginService.checkUserLogin(key)) {
             Appointment registerAppointment = userService.bookAppointment(key, appointment); //Proceeds to book appointment
             return new ResponseEntity<Appointment>(registerAppointment, HttpStatus.CREATED);
-        }else {
+        } else {
             throw new LoginException("Please Login Again.");
         }
     }
 
     @GetMapping("/allConsultants")
     @CrossOrigin
-    public ResponseEntity<List<Consultant>> getAllConsultantsInDatabase (@RequestParam String key) throws LoginException,
-            ConsultantException{
-        if (userAdminLoginService.checkUserLogin(key)){
+    public ResponseEntity<List<Consultant>> getAllConsultantsInDatabase(@RequestParam String key) throws LoginException,
+            ConsultantException {
+        if (userAdminLoginService.checkUserLogin(key)) {
             List<Consultant> listOfConsultants = consultantService.getAllConsultantsInDb(); //Retrieve a list of consultants
-                                                                                            // from the database
+            // from the database
             return new ResponseEntity<List<Consultant>>(listOfConsultants, HttpStatus.ACCEPTED);
-        }else {
+        } else {
             throw new LoginException("Invalid Credentials; Please Login.");
         }
     }
@@ -92,17 +93,69 @@ public class UserController {
     @GetMapping("/getAllConsultants")
     @CrossOrigin
     public ResponseEntity<List<Consultant>> getAllConsultants(@RequestParam String key) throws
-            LoginException, ConsultantException{
-        if (userAdminLoginService.checkUserLogin(key)){  //Retrieves list of consultants from the userService
+            LoginException, ConsultantException {
+        if (userAdminLoginService.checkUserLogin(key)) {  //Retrieves list of consultants from the userService
             List<Consultant> consultantList = userService.getAllConsultant();
             return new ResponseEntity<List<Consultant>>(consultantList, HttpStatus.ACCEPTED);
-        }else {
+        } else {
             throw new LoginException("Invalid Credentials; Please Login.");
         }
     }
 
+    @GetMapping("/allAppointment")
+    @CrossOrigin
+    public ResponseEntity<List<Appointment>> getAllAppointments(@RequestParam String key) throws
+            AppointmentException, UserException, LoginException {
+        if (userAdminLoginService.checkUserLogin(key)) {
+            List<Appointment> listOfAppointments = userService.getUserAppointment(key);  //Retrieves a list of appointments for the user
+            return new ResponseEntity<List<Appointment>>(listOfAppointments, HttpStatus.ACCEPTED);
+        } else {
+            throw new LoginException("Please Login Again.");
+        }
+    }
 
+    @DeleteMapping("/cancelAppointment")
+    @CrossOrigin
+    public ResponseEntity<Appointment> deleteAppointmet(@RequestParam String key, @RequestBody Appointment appointment)
+            throws AppointmentException, ConsultantException, Exception {
+        if (userAdminLoginService.checkUserLogin(key)) {
+            Appointment consultantList = userService.deleteAppointment(appointment);
+            return new ResponseEntity<Appointment>(consultantList, HttpStatus.ACCEPTED);
+        } else {
+            throw new LoginException("Please Login Again.");
+        }
+    }
+
+    @PutMapping("/forgotPassword")
+    @CrossOrigin
+    public ResponseEntity<User> forgotPassword(@RequestParam String key, @RequestBody ForgotPassword forgotPassword)
+            throws LoginException, PasswordException {
+        if (userAdminLoginService.checkUserLogin(key)) {
+            if (forgotPassword.getNewPassword().equals(forgotPassword.getConfirmNewPassword())) {  //Proceeds to check if
+                                                                    // the new password and the confirm new password match
+
+                if (forgotPassword.getCurrentPassword().equals(forgotPassword.getNewPassword())) {  //If the passwords match,
+                                                     // checks if the current password is the same as the new password.
+                    throw new PasswordException("Please Enter New Password.");
+                }
+                User finalResult = userService.forgotPassword(key, forgotPassword);
+                return new ResponseEntity<User>(finalResult, HttpStatus.ACCEPTED);
+            } else {
+                throw new PasswordException("Passwords Does Not Matched. Please Try Again.");
+            }
+        } else {
+            throw new LoginException("Invalid Credentials. Please Login Again.");
+        }
+    }
 }
+
+
+
+
+
+
+
+
 
 
 
