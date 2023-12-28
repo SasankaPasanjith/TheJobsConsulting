b9075a4f24d1aaed81e1f41c76fb4f979af9144e
SasankaPasanjith <sasankapasanjith@gmail.com>
2023-12-28 12:44:42 +0530

    added Comment Exception.

---
+++ src/main/java/com/TheJobsConsulting/exception/Commentsexception.java
@@ -0,0 +1,11 @@
+package com.TheJobsConsulting.exception;
+
+public class Commentsexception extends Exception {
+    public Commentsexception() {
+    }
+
+    public Commentsexception(String msg) {
+        super(msg);
+    }
+}
+
