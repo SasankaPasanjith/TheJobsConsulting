da288ac5a926e990260900abb1b9cd08d935c802
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-01-05 12:52:56 +0530

    Edited GlobalExceptionHandler class

--- src/main/java/com/TheJobsConsulting/exception/GlobalExceptionHandler.java
+++ src/main/java/com/TheJobsConsulting/exception/GlobalExceptionHandler.java
@@ -3,11 +3,15 @@ package com.TheJobsConsulting.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -65,7 +69,36 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<MyErrorDetails>(myErrorDetails, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler (MethodArgumentNotValidException.class)
+    public ResponseEntity<MyErrorDetails> constraintExceptionHandler (MethodArgumentNotValidException ConstraintViolationException,
+                                                                      WebRequest webRequest){
+        List<String> details = new ArrayList<>(); // Extracting error details from the exception
+        for (ObjectError error : ConstraintViolationException.getBindingResult().getAllErrors()){
+            details.add(error.getDefaultMessage());
+        }
+        MyErrorDetails myErrorDetails = new MyErrorDetails(); // Creating an instance of MyErrorDetails to encapsulate error information
+        myErrorDetails.setDetails(webRequest.getDescription(false));
+        myErrorDetails.setErrorMsg(details.get(0));
+        myErrorDetails.setLocalDateTime(LocalDateTime.now());
+
+        return new ResponseEntity<MyErrorDetails>(myErrorDetails, HttpStatus.BAD_REQUEST); // Returning a ResponseEntity
+                                                                  // with MyErrorDetails and HTTP status code BAD_REQUEST
+    }
+
+    @ExceptionHandler (Exception.class)
+    public ResponseEntity<MyErrorDetails> handleAllException (Exception exception, WebRequest webRequest){
+        MyErrorDetails myErrorDetails = new MyErrorDetails();
+
+        myErrorDetails.setDetails(webRequest.getDescription(false));
+        myErrorDetails.setErrorMsg(exception.getMessage());
+        myErrorDetails.setLocalDateTime(LocalDateTime.now());
+
+        return new ResponseEntity<MyErrorDetails>(myErrorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
+
+
 
 
 
