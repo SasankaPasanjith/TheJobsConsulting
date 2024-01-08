cadd9f4c824f82e3696f17124419b3e5b06e8255
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-01-08 20:24:47 +0530

    Initial Commit

--- src/test/java/com/TheJobsConsulting/UserControllerTest.java
+++ src/test/java/com/TheJobsConsulting/UserControllerTest.java
@@ -1,4 +1,16 @@
 package com.TheJobsConsulting;
 
+import com.TheJobsConsulting.controller.UserController;
+import com.TheJobsConsulting.service.UserService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
 public class UserControllerTest {
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+
 }
