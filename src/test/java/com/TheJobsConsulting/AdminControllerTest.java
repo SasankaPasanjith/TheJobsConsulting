024475e3cbce4cc0cc9518a1112fbac07dfa6530
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-01-09 13:46:55 +0530

    Admin Controller test

---
+++ src/test/java/com/TheJobsConsulting/AdminControllerTest.java
@@ -0,0 +1,32 @@
+package com.TheJobsConsulting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTest {
+
+    @Test
+    void registerConsultant() {
+    }
+
+    @Test
+    void getAllConsultants() {
+    }
+
+    @Test
+    void getALlUsers() {
+    }
+
+    @Test
+    void getAllAppointment() {
+    }
+
+    @Test
+    void revokePermissionConsultant() {
+    }
+
+    @Test
+    void grantPermissionConsultant() {
+    }
+}
