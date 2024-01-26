b15b08393eed42c4ef3602c26a18a5b5037c13f1
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-01-26 12:27:37 +0530

    Added test case for AdmincontrollerTest.

--- src/test/java/com/TheJobsConsulting/AdminControllerTest.java
+++ src/test/java/com/TheJobsConsulting/AdminControllerTest.java
@@ -1,14 +1,63 @@
 package com.TheJobsConsulting;
 
+import com.TheJobsConsulting.controller.AdminController;
+import com.TheJobsConsulting.entity.Consultant;
+import com.TheJobsConsulting.entity.CurrentSession;
+import com.TheJobsConsulting.exception.ConsultantException;
+import com.TheJobsConsulting.exception.LoginException;
+import com.TheJobsConsulting.service.AdminService;
+import com.TheJobsConsulting.service.UserAdminLoginService;
+import com.TheJobsConsulting.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-class AdminControllerTest {
+public class AdminControllerTest {
+
+    @InjectMocks
+    private AdminController adminController;
+
+    @Mock
+    private AdminService adminService;
+
+    @Mock
+    private UserAdminLoginService userAdminLoginService;
+
+    @Mock
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
-    void registerConsultant() {
+    public void registerConsultant() throws ConsultantException, LoginException {
+        when (userAdminLoginService.checkUserLogin(anyString())).thenReturn(true);
+        CurrentSession currentUserSession = new CurrentSession();
+        currentUserSession.setUserType("admin");
+        when(userService.getCurrentUserByUuid(anyString())).thenReturn(currentUserSession);
+
+        Consultant consultant = new Consultant();
+        when (adminService.registerConsultant(any (Consultant.class))).thenReturn(consultant);
+
+        ResponseEntity<Consultant> responce = adminController.registerConsultant("valid_key", consultant);
+        assertEquals(HttpStatus.CREATED, responce.getStatusCode());
+        assertEquals(consultant, responce.getBody());
+
+        verify(userAdminLoginService).checkUserLogin("valid_key");
+        verify(userService).getCurrentUserByUuid("valid_key");
+        verify(adminService).registerConsultant(consultant);
     }
+
+
 
     @Test
     void getAllConsultants() {
